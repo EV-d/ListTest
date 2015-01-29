c671cc91fa601d0d5929106b609ff9f776eedc8a
EV-d <haitham11228@cceoman.net>
2015-01-29 11:09:10 +0400

    Update MyActivity.java

--- app/src/main/java/net/ssp/listtest/MyActivity.java
+++ app/src/main/java/net/ssp/listtest/MyActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-
+//to test
 
 public class MyActivity extends Activity {
 
